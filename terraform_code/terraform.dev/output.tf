9a0b3f8e7614aafecb29020eadd5e126083afeae
Gagan Kalra <gagandeep.singh@logrhythm.com>
2021-08-15 13:08:58 +0530

    adding to terrraform igle

--- terraform_code/terraform.dev/output.tf
+++ terraform_code/terraform.dev/output.tf
@@ -10,3 +10,7 @@ output "Instance_IP" {
   value = aws_instance.mysql-instance.public_ip
 }
 
+resource "local_file" "Instance_IP_Out" {
+    content  = aws_instance.mysql-instance.public_ip
+    filename = "instance_ip_dev"
+}
