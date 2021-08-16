371d60fa9b34ee546a0797a5f0c5862dbaf5b8d7
Gagan Kalra <gagandeep.singh@logrhythm.com>
2021-08-16 20:48:00 +0530

    terraform varsssssssss

--- terraform_code/terraform.dev/output.tf
+++ terraform_code/terraform.dev/output.tf
@@ -1,11 +1,4 @@
-output "Instance_ID" {
-  value = aws_instance.mysql-instance.id
-}
-
-output "Instance_DNS" {
-  value = aws_instance.mysql-instance.public_dns
-}
-
 output "Instance_IP" {
   value = aws_instance.mysql-instance.public_ip
+  description = "The IP of the MySQL machine."
 }
--- terraform_code/terraform.prod/vars.tf
+++ terraform_code/terraform.prod/vars.tf
@@ -1,18 +1,22 @@
 variable "vpc_id" {
+  type = string
   description = "VPC ID"
   default     = "vpc-c8c44ab5"
 }
 variable "igw_id" {
+  type = string
   description = "IGW ID"
   default     = "igw-2dba4757"
 }
 variable "subnet1_cidr" {
+  type = string
   description = "The CIDR block for the subnet"
   default     = "172.31.112.0/20"
 }
 variable "az1" {
   description = "The availability zone for the subnet"
   default     = "us-east-1a"
+  type = string
 }
 variable "map_public_ip" {
   type        = bool
@@ -47,6 +51,35 @@ variable "instance_type" {
   default     = "t2.micro"
 }
 variable "key_name" {
+  type = string
   description = "key name"
   default     = "gagan-keys"
 }
+
+variable "sg_tag_name" {
+  type = string
+  description = "Security Group tag name"
+  default     = "team4_mysql_prod_sg"
+}
+variable "subnet_tag_name" {
+  type = string
+  description = "Subnet tag name"
+  default     = "team4_mysql_prod_subnet"
+}
+variable "vm_tag_name" {
+  description = "VM tag name"
+  type = string
+  default     = "team4_MySQL_host_prod_instance"
+}
+variable "env_tag_name" {
+  description = "ENV tag name"
+  type = string
+  default     = "prod"
+}
+variable "sg_resource_name" {
+  description = " Resource name"
+  type = string
+  default     = "team4-mysql-prod-sg"
+}
+
+
